8bf9674521a138e1abfb248fede952821a2c635f
Carlos Alvarado <c.alvarado_@hotmail.com>
2024-04-25 17:43:11 -0500

    adding GoogleAnalyticsScript

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import AuthProvider from "./auth/Provider";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import { GoogleAnalyticsScript } from "@/GoogleAnalyticsScript";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
+      <GoogleAnalyticsScript />
       <body className={inter.className}>
         <AuthProvider>
           <NavBar />
