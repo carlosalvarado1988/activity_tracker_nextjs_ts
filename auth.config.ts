7676bc04a5324740eb55395d948ecc3626f65b28
Carlos Alvarado <c.alvarado_@hotmail.com>
2024-04-25 15:08:34 -0500

    adding database adapter and credentials provider to authjs config to allow user and password login

---
+++ auth.config.ts
@@ -0,0 +1,43 @@
+import type { NextAuthConfig } from "next-auth";
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+
+export default {
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email as string },
+        });
+        if (!user) return null;
+        const bcrypt = require("bcrypt");
+        const passwordsMatched = await bcrypt.compare(
+          credentials.password as string,
+          user.hashedPassword!
+        );
+        return passwordsMatched ? user : null;
+        // return user;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
