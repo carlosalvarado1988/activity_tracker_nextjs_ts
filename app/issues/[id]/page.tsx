e226142f48e7b1e1351f4b3f7767fe7eb895c0e3
Carlos Alvarado <c.alvarado_@hotmail.com>
2024-05-15 22:56:06 -0400

    adding the edit button with redix icon

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
-import { Link, Button } from "@radix-ui/themes";
+import { Box, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import Link from "next/link";
 interface Props {
   params: { id: string };
 }
@@ -17,19 +20,31 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <section className="max-w-xl">
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="3">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mb="3">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
+    <section>
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Box>
+          <Heading>{issue.title}</Heading>
+          <Flex gap="3" my="3">
+            <IssueStatusBadge status={issue.status} />
+            <Text>{issue.createdAt.toDateString()}</Text>
+          </Flex>
+          <Card className="prose" mb="3">
+            <ReactMarkdown>{issue.description}</ReactMarkdown>
+          </Card>
 
-      <Link href={"/issues"}>
-        <Button>Back</Button>
-      </Link>
+          <Link href={"/issues"}>
+            <Button>Back</Button>
+          </Link>
+        </Box>
+        <Box>
+          <Link href={`issues/${issue.id}/edit`}>
+            <Button>
+              <Pencil1Icon />
+              Edit Issue
+            </Button>
+          </Link>
+        </Box>
+      </Grid>
     </section>
   );
 };
