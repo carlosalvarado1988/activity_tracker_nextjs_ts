5450190c3154b5eb9e0b832f40f276c2fe788e67
Carlos Alvarado <c.alvarado_@hotmail.com>
2024-05-29 19:33:04 -0400

    updaing PATH endpoint to issues to allow for assignedToUserId and flexible/optional values in the patch call

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { issueSchema } from "../../utils";
+import { patchIssueSchema } from "../../utils";
 
 export async function PATCH(
   request: NextRequest,
@@ -15,7 +15,7 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
@@ -26,9 +26,21 @@ export async function PATCH(
   if (!issue)
     return NextResponse.json({ error: "Issue not found" }, { status: 401 });
 
+  const { assignedToUserId, title, description } = body;
+
+  if (assignedToUserId) {
+    // if we are going to assing, first check if the user is valid to continue
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 401 });
+  }
+
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title!, description: body.description },
+    data: { title, description, assignedToUserId },
   });
 
   return NextResponse.json(updatedIssue, { status: 200 });
